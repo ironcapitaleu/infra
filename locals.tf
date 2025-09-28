d5aa9e24ae09923f018e12c0ff0be659cce126a3
Demir Catovic <d.catovic9@gmail.com>
2025-09-28 14:33:50 +0200

    refactor: define some constants in `locals.tf`

--- locals.tf
+++ locals.tf
@@ -3,4 +3,19 @@ locals {
   # TODO: Will be used for environment-specific configurations
   # tflint-ignore: terraform_unused_declarations
   environment = var.environment
+
+
+  # SSL/TLS & HTTPS Settings
+  ssl_settings = {
+    ssl_mode                 = "strict"
+    always_use_https         = "on"
+    automatic_https_rewrites = "on"
+  }
+
+  # Security & WAF Settings
+  security_settings = {
+    browser_check  = "on"
+    security_level = "medium"
+  }
+
 }
