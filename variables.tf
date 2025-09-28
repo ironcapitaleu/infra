d83aadc080d741906276876191571953c58b7696
Demir Catovic <d.catovic9@gmail.com>
2025-09-28 15:36:03 +0200

    feat: add validation to variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,22 @@
 variable "environment" {
   description = "Environment (e.g., `development`, `production`, ...)"
   type        = string
+
+  validation {
+    condition = contains([
+      "development",
+      "production"
+    ], var.input_environment)
+    error_message = "Environment must be one of: `development`, `production`"
+  }
 }
 
 variable "zone_id" {
   description = "Cloudflare Zone ID"
   type        = string
+
+  validation {
+    condition     = can(regex("^[a-f0-9]{32}$", var.input_zone_id))
+    error_message = "Zone ID must be a valid 32-character hexadecimal string"
+  }
 }
