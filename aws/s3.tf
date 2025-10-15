8715c4f683a337d527216b803590c1a13b6da9bb
Demir Catovic <d.catovic9@gmail.com>
2025-10-16 00:18:27 +0200

    fix: add public access block to origin bucket

--- aws/s3.tf
+++ aws/s3.tf
@@ -16,10 +16,10 @@ resource "aws_s3_bucket_versioning" "cloudfront_origin" {
 resource "aws_s3_bucket_public_access_block" "cloudfront_origin" {
   bucket = aws_s3_bucket.cloudfront_origin.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_website_configuration" "cloudfront_origin" {
