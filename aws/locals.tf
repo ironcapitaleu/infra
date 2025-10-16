c032608d6b1becfcaba7185db91cf592cc80dbda
Demir Catovic <d.catovic9@gmail.com>
2025-10-16 11:53:07 +0200

    fix: syntax for s3 bucket locals

--- aws/locals.tf
+++ aws/locals.tf
@@ -1,7 +1,7 @@
 locals {
   environment = var.environment
 
-  s3_buckets {
+  s3_buckets = {
     cloudfront_origin = "arkad-frontend-cloudfront-origin-${local.environment}"
     cloudfront_logs   = "arkad-frontend-cloudfront-logs-${local.environment}"
     access_logs       = "arkad-frontend-access-logs-${local.environment}"
