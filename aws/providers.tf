8b71e0305e9359bd26bcef2d4d216444f65dbb8d
Demir Catovic <d.catovic9@gmail.com>
2025-10-16 12:43:25 +0200

    feat: add backend config for `aws` backend

--- aws/providers.tf
+++ aws/providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   # AWS credentials and region will be read from environment variables
 }
+
+terraform {
+  # S3 backend configuration
+  backend "s3" {
+    key            = "aws/terraform.tfstate"
+    dynamodb_table = "terraform-aws-state-lock"
+    encrypt        = true
+  }
+}
