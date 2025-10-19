c64d29786dd2b51473f195a0932593d602c31e62
Demir Catovic <d.catovic9@gmail.com>
2025-10-19 13:28:06 +0200

    fix: use relative path inside module

--- aws/modules/cloudfront-spa/s3.tf
+++ aws/modules/cloudfront-spa/s3.tf
@@ -246,7 +246,7 @@ resource "null_resource" "build_spa" {
 resource "aws_s3_object" "spa_files" {
   depends_on = [null_resource.build_spa]
 
-  for_each = fileset("${var.frontend.path}/dist", "**")
+  for_each = fileset("../../${var.frontend.path}/dist", "**")
 
   bucket = aws_s3_bucket.cloudfront_origin.id
   key    = each.value
