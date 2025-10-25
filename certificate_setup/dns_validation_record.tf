94a2baee8da362cd75d013f638ba20e88fdd6d0e
Demir Catovic <d.catovic9@gmail.com>
2025-10-25 23:40:54 +0200

    fix: correct resource type and add lifecycle preconditions

--- certificate_setup/dns_validation_record.tf
+++ certificate_setup/dns_validation_record.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_record" "acm_validation" {
+resource "cloudflare_dns_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
       name  = dvo.resource_record_name
@@ -13,4 +13,18 @@ resource "cloudflare_record" "acm_validation" {
   value   = each.value.value
   ttl     = 1
   proxied = false
+
+  lifecycle {
+    precondition {
+      condition = contains([
+        "A", "AAAA", "CNAME", "MX", "TXT", "SRV", "NS", "PTR"
+      ], each.value.type)
+      error_message = "DNS record type must be valid for record `${each.key}`. Needs to be one of: {`A`, `AAAA`, `CNAME`, `MX`, `TXT`, `SRV`, `NS`, `PTR`}. Got `${each.value.type}` instead."
+    }
+
+    precondition {
+      condition     = (each.value.ttl == 1) || (each.value.ttl >= 60 && each.value.ttl <= 86400)
+      error_message = "TTL should be exactly `1` (automatic, e.g., if proxied record) or between `60` and `86400` seconds for record `${each.key}`. Got `${each.value.ttl}` instead."
+    }
+  }
 }
