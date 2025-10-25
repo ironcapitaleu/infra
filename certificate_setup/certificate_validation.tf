a0b7e0ef9ea94f567462877d5de2dc070755bab5
Demir Catovic <d.catovic9@gmail.com>
2025-10-25 23:42:45 +0200

    fix: update to use correct resource reference

--- certificate_setup/certificate_validation.tf
+++ certificate_setup/certificate_validation.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate_validation" "this" {
   provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [for record in cloudflare_record.acm_validation : record.hostname]
+  validation_record_fqdns = [for record in cloudflare_dns_record.acm_validation : record.hostname]
 }
