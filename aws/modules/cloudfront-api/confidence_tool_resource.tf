a4ba5bc08b2cc8e0fe6abe9347889988fdc82614
Demir Catovic <d.catovic9@gmail.com>
2025-10-26 23:22:32 +0100

    feat: add `confidence-tool` resource

---
+++ aws/modules/cloudfront-api/confidence_tool_resource.tf
@@ -0,0 +1,80 @@
+# -------------------------
+# API Gateway (Mock /confidence-tool)
+# -------------------------
+
+resource "aws_api_gateway_resource" "confidence_tool_resource" {
+  rest_api_id = aws_api_gateway_rest_api.simple_api.id
+  parent_id   = aws_api_gateway_rest_api.simple_api.root_resource_id
+  path_part   = "confidence-tool"
+}
+
+resource "aws_api_gateway_method" "get_confidence_tool" {
+  rest_api_id      = aws_api_gateway_rest_api.simple_api.id
+  resource_id      = aws_api_gateway_resource.confidence_tool_resource.id
+  http_method      = "GET"
+  authorization    = "NONE"
+  api_key_required = false
+}
+
+resource "aws_api_gateway_integration" "mock_integration" {
+  rest_api_id          = aws_api_gateway_rest_api.simple_api.id
+  resource_id          = aws_api_gateway_resource.confidence_tool_resource.id
+  http_method          = aws_api_gateway_method.get_confidence_tool.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "mock_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.simple_api.id
+  resource_id = aws_api_gateway_resource.confidence_tool_resource.id
+  http_method = aws_api_gateway_method.get_confidence_tool.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = "{\"message\": \"Hello from the Confidence Tool!\"}"
+  }
+
+  # Return CORS header for all responses
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.mock_integration]
+}
+
+resource "aws_api_gateway_method_response" "get_confidence_tool_response" {
+  rest_api_id = aws_api_gateway_rest_api.simple_api.id
+  resource_id = aws_api_gateway_resource.confidence_tool_resource.id
+  http_method = aws_api_gateway_method.get_confidence_tool.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.simple_api.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  triggers = {
+    redeployment = timestamp()
+  }
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id   = aws_api_gateway_rest_api.simple_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  stage_name    = var.api_gateway.stage_name
+}
