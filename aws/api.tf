99f51da87a7eec2cb54030e107f352ada66f9356
Demir Catovic <d.catovic9@gmail.com>
2025-10-26 22:29:37 +0100

    fix: add proper input vars to module call + remove unnecessary `cd` steps for testing

--- aws/api.tf
+++ aws/api.tf
@@ -1,10 +1,8 @@
 module "cloudfront-api" {
   source = "./modules/cloudfront-api"
 
-  s3_buckets = local.spa_deployment.s3_buckets
-
-  cloudfront_distribution = local.spa_deployment.cloudfront_distribution
-
-  frontend = local.spa_deployment.frontend
+  api_gateway = local.api_backend.api_gateway
+  
+  cloudfront_distribution = local.api_backend.cloudfront_distribution
 
 }
