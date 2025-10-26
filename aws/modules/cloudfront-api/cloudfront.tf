b0cf0c2399e32c415e6d9c23f8555e881a473f34
Demir Catovic <d.catovic9@gmail.com>
2025-10-26 22:36:06 +0100

    fix: remove `reponse_code` from `custom_error_response`

--- aws/modules/cloudfront-api/cloudfront.tf
+++ aws/modules/cloudfront-api/cloudfront.tf
@@ -21,7 +21,6 @@ resource "aws_cloudfront_distribution" "api_distribution" {
     target_origin_id       = var.cloudfront_distribution.origin_id
     viewer_protocol_policy = "redirect-to-https"
 
-    # Managed policy IDs
     cache_policy_id            = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
     origin_request_policy_id   = "b689b0a8-53d0-40ab-baf2-68738e2966ac" # AllViewerExceptHostHeader
     response_headers_policy_id = "eaab4381-ed33-4a86-88ca-d9558dc6cd63" # CORS-with-preflight-and-SecurityHeadersPolicy
@@ -29,7 +28,6 @@ resource "aws_cloudfront_distribution" "api_distribution" {
 
   custom_error_response {
     error_code            = 501
-    response_code         = 501
     error_caching_min_ttl = 0
   }
 
