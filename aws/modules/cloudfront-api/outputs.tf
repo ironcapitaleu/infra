600f649b4da1d14dbef96d447735b8b39986fea3
Demir Catovic <d.catovic9@gmail.com>
2025-10-27 10:14:36 +0100

    refactor: rename api endpoint resource

--- aws/modules/cloudfront-api/outputs.tf
+++ aws/modules/cloudfront-api/outputs.tf
@@ -1,11 +1,11 @@
 output "rest_api_id" {
   description = "API Gateway REST API ID"
-  value       = aws_api_gateway_rest_api.simple_api.id
+  value       = aws_api_gateway_rest_api.api_endpoint.id
 }
 
 output "api_gateway_id" {
   description = "Base invoke URL for the API Gateway"
-  value       = aws_api_gateway_rest_api.simple_api.id
+  value       = aws_api_gateway_rest_api.api_endpoint.id
 }
 
 output "stage_name" {
